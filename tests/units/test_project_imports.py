f55cb25f42ed9ec7881a073701debc5540bcba0c
Hugo Perrin <hugo1344.perrin@gmail.com>
2021-04-26 19:09:25 +0200

    feat: specialized project for client for mitsuba

--- tests/units/test_project_imports.py
+++ tests/units/test_project_imports.py
@@ -3,9 +3,7 @@ import unittest
 
 class TestImports(unittest.TestCase):
     def test_module_imports(self):
-        import rl_sampling.data as data
-        import rl_sampling.experiments as exp
-        import rl_sampling.models as models
-        import rl_sampling.utils as utils
-
+        import mitsuba_client.cli as cli
+        import mitsuba_client.client as client
+        import mitsuba_client.utils as utils
         self.assertTrue(True)
