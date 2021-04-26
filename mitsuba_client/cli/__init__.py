98b293db6d83a01caa7b378e9d14a1e9a9365c59
hugoperrin <hugo1344.perrin@gmail.com>
2021-04-26 19:11:34 +0200

    feat: setup the project (#1)
    * feat: add basic testing and linting scripts
    
    * feat: specialized project for client for mitsuba
    
    * feat: specialized project for client for mitsuba
    
    * feat: specialized project for client for mitsuba

---
+++ mitsuba_client/cli/__init__.py
@@ -0,0 +1,1 @@
+"""Module for the cli usage"""
---
+++ tests/units/test_project_imports.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestImports(unittest.TestCase):
+    def test_module_imports(self):
+        import mitsuba_client.cli as cli
+        import mitsuba_client.client as client
+        import mitsuba_client.utils as utils
+        self.assertTrue(True)
