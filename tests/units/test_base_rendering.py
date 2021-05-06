6347e666f21a243eab2e43e96ccb31ad4bce0b1f
Hugo Perrin <hugo1344.perrin@gmail.com>
2021-05-06 19:59:03 +0200

    feat: gaa

---
+++ tests/units/test_base_rendering.py
@@ -0,0 +1,23 @@
+"""Test base rendering on cpu."""
+import os
+import unittest
+from typing import List
+
+import numpy as np
+
+import mitsuba
+from mitsuba_client.client.rendering_env import RendererEnv
+
+mitsuba.set_variant("packet_rgb")
+
+
+class TestBaseRendering(unittest.TestCase):
+    def test_rendering(self):
+        path_scene: str = os.path.join("scenes", "bathroom", "scene.xml")
+        env: RendererEnv = RendererEnv()
+        env.load_scene(scene_path=path_scene, scene_id="test")
+        imgs: List = env.render("test")
+        self.assertIsInstance(imgs, list)
+        for img in imgs:
+            self.assertIsInstance(img, np.ndarray)
+            self.assertTrue(img.shape == (512, 512, 3))
